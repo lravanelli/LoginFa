4430239efc5340f4b0b1d3e08176a2d03eba54f4
lravanelli <ravanelli.luis@gmail.com>
2018-04-07 13:10:45 -0300

    Atu.

--- app/src/main/java/br/bom/fiap/loginfa/LoginActivity.kt
+++ app/src/main/java/br/bom/fiap/loginfa/LoginActivity.kt
@@ -2,11 +2,49 @@ package br.bom.fiap.loginfa
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.FacebookSdk
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.FacebookCallback
+import com.facebook.login.LoginManager
+import com.facebook.CallbackManager
+import android.content.Intent
+
+
+
+
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+        callbackManager = CallbackManager.Factory.create()
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(loginResult: LoginResult) {
+
+                    }
+
+                    override fun onCancel() {
+
+                    }
+
+                    override fun onError(exception: FacebookException) {
+
+                    }
+                })
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
